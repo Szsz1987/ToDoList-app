a5b09366fbf88f9a742f5c6c59ac151680a0a40c
Szymon <sszulinski@gmail.com>
2021-06-20 14:44:01 +0200

    Add client view

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
 const app = express();
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+app.use(cors());
+
+let tasks = [];
+
+app.use((req,res) => {
+  return res.status(404).json({
+    message: 'Not found...',
+  });
 });
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
+const io = socket(server);
